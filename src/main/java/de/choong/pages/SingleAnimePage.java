15d9ffff649fe57c3fca076643fb912eabd50f0c
Kchoong <cehvierdeh-github@yahoo.com>
2015-04-03 22:01:04 +0200

    Style and pageParameters.

--- src/main/java/de/choong/pages/SingleAnimePage.java
+++ src/main/java/de/choong/pages/SingleAnimePage.java
@@ -1,19 +1,34 @@
 package de.choong.pages;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import de.choong.dao.IAnimeDao;
+import de.choong.exceptions.DBException;
+import de.choong.model.AnimeDO;
+import de.choong.util.SpringUtil;
 
 public class SingleAnimePage extends BasePage {
 
-    public SingleAnimePage() {
-        super();
-    }
+    private static final long serialVersionUID = -8384777450358070031L;
 
+    private IAnimeDao dao = (IAnimeDao) SpringUtil.getBean("animeDao");
+    private AnimeDO anime;
+
+    // TODO ID NULL OR NOT INT
     public SingleAnimePage(final PageParameters parameters) {
-        super(parameters);
+        int id = parameters.get("id").toInt();
+        try {
+            anime = dao.read(id);
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
+        // TODO fill information
+        add(new Label("title", anime.getTitle()));
     }
 }
