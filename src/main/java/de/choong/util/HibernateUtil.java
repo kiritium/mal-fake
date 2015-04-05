c343e027f1ee4dc4f8909e97111cbf51b6a21629
Kchoong <cehvierdeh-github@yahoo.com>
2015-04-05 21:39:22 +0200

    Experimental onCreate + onUpdate.

--- src/main/java/de/choong/util/HibernateUtil.java
+++ src/main/java/de/choong/util/HibernateUtil.java
@@ -1,11 +1,17 @@
 package de.choong.util;
 
+import java.io.Serializable;
+
+import org.hibernate.EmptyInterceptor;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import org.hibernate.type.Type;
+
+import de.choong.model.BaseDO;
 
 /**
  * Util to access DB operations.
@@ -22,7 +28,8 @@ public class HibernateUtil {
      */
     public static SessionFactory getSessionFactory() throws HibernateException {
         if (sessionFactory == null) {
-            Configuration conf = new Configuration().configure();
+            Configuration conf = new Configuration().configure().setInterceptor(
+                    new AuditInterceptor());
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
                     conf.getProperties()).build();
             sessionFactory = conf.buildSessionFactory(serviceRegistry);
@@ -37,5 +44,36 @@ public class HibernateUtil {
      */
     public static Session getSession() {
         return getSessionFactory().openSession();
+    }
+
+    private static class AuditInterceptor extends EmptyInterceptor {
+        private static final long serialVersionUID = 1094556730902967262L;
+
+        public AuditInterceptor() {
+            super();
+        }
+
+        @Override
+        public boolean onSave(Object entity, Serializable id, Object[] state,
+                String[] propertyNames, Type[] types) {
+            if (entity instanceof BaseDO) {
+                BaseDO e = (BaseDO) entity;
+                e.onCreate();
+                System.out.println("created");
+            }
+            return super.onSave(entity, id, state, propertyNames, types);
+        }
+
+        @Override
+        public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState,
+                Object[] previousState, String[] propertyNames, Type[] types) {
+            if (entity instanceof BaseDO) {
+                BaseDO e = (BaseDO) entity;
+                e.onUpdate();
+                System.out.println("update");
+            }
+            return super
+                    .onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+        }
     }
 }
