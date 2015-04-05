d16ea8c7059c138673c7add6f467e5ccdb5025e0
Billy Duong <flamellfelix-github@yahoo.de>
2015-04-05 14:47:04 +0200

    Fixed FeedbackPanel. FeedbackPanel wasn't showing some messages. Also changed LoginForm's feedback to our standard feedback panel.

--- src/main/java/de/choong/form/LoginForm.java
+++ src/main/java/de/choong/form/LoginForm.java
@@ -2,12 +2,16 @@ package de.choong.form;
 
 import org.apache.wicket.authroles.authentication.panel.SignInPanel;
 
+import de.choong.components.AjaxFeedbackPanel;
+
 public class LoginForm extends SignInPanel {
 
     private static final long serialVersionUID = -2072142982507657566L;
 
     public LoginForm(String id) {
         super(id);
+
+        addOrReplace(new AjaxFeedbackPanel("feedback"));
     }
 
 }
