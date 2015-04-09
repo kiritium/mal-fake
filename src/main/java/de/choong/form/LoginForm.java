e4ae2015da518515f1c979665f39f8d6a4039150
Kchoong <cehvierdeh-github@yahoo.com>
2015-04-09 19:06:42 +0200

    Wicket-label LoginForm.

--- src/main/java/de/choong/form/LoginForm.java
+++ src/main/java/de/choong/form/LoginForm.java
@@ -1,6 +1,9 @@
 package de.choong.form;
 
 import org.apache.wicket.authroles.authentication.panel.SignInPanel;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
 import de.choong.components.AjaxFeedbackPanel;
 
@@ -10,8 +13,12 @@ public class LoginForm extends SignInPanel {
 
     public LoginForm(String id) {
         super(id);
+        @SuppressWarnings("unchecked")
+        TextField<String> txt = (TextField<String>) get("signInForm").get("username");
+        txt.setLabel(Model.of("Username"));
 
+        PasswordTextField pw = (PasswordTextField) get("signInForm").get("password");
+        pw.setLabel(Model.of("Password"));
         addOrReplace(new AjaxFeedbackPanel("feedback"));
     }
-
 }
